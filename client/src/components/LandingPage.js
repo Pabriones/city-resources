d9e8c528f6dbc72ea4c768853599b7f6a087cd8a
selinapn <57070340+selinapn@users.noreply.github.com>
2020-10-25 22:03:47 -0700

    landing page
    unfinished

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../../src/css/LandingPage.css";
+import { Icon } from "semantic-ui-react";
+
+const LandingPage = () => {
+  return (
+    <body>
+      <div class="container">
+        <div class="background">
+          <div class="title">CITY RESOURCES</div>
+          <div class="nav">
+            <div class="nav">
+              <a href="url">ABOUT </a>
+              <a href="url">CONTACT</a>
+
+           <div> <Icon>
+              <a href=" ">
+                <button type="button" class="searchDirectoryBtn">
+                  <p class= "searchText">SEARCH DIRECTORY <ion-icon
+                    class="chevIconSearch"
+                    name="chevron-forward-outline"
+                  ></ion-icon></p>
+                 
+                </button>
+              </a>
+            </Icon></div>
+
+
+            <Icon>
+              <a href=" ">
+                <button type="button" class="emergencyLinesBtn">
+                  <p class="emergenText">EMERGENCY AND CRISIS LINES <ion-icon
+                    class="chevIconLines"
+                    name="chevron-forward-outline"
+                  ></ion-icon></p>
+                  
+                </button>
+              </a>
+            </Icon></div>
+          </div>
+        </div> 
+      </div>
+    </body>
+  );
+};
+
+export default LandingPage;

2ea64579b669679397f40900b79fdc31a4200a2b
MargaritaYatina <56407643+MargaritaYatina@users.noreply.github.com>
2021-01-14 22:10:35 -0800

    Contact and About buttons redirect to AboutPage and ContactPage

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
 import ContactPage from "./components/ContactPage";
+import AboutPage from "./components/AboutPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-      <div>
-        <ContactPage />
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={LandingPage} exact />
+
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+      </Switch>
+    </Router>
   );
 }
 

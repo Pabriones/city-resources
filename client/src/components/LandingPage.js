ab9d51c166538a9b508983387ce414f36fcb808f
selinapn <57070340+selinapn@users.noreply.github.com>
2021-01-21 21:12:11 -0800

    lp styling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -5,45 +5,41 @@ import { Icon } from "semantic-ui-react";
 const LandingPage = () => {
   return (
     <body>
-      <div class="container">
-        <div class="background">
-          <div class="title">CITY RESOURCES</div>
-          <div class="nav">
-            <div class="nav">
-              <a href="url">ABOUT </a>
-              <a href="url">CONTACT</a>
-            </div>
-            <div class="flex-container">
-              <div>
-                <Icon>
-                  <a href=" ">
-                    <button type="button" class="searchDirectoryBtn">
-                      <p class="searchText">
-                        SEARCH DIRECTORY
-                        <ion-icon
-                          class="chevIconSearch"
-                          name="chevron-forward-outline"
-                        ></ion-icon>
-                      </p>
-                    </button>
-                  </a>
-                </Icon>
-              </div>
+      <div class="lp-container">
+        <div class="lp-title">CITY RESOURCES</div>
 
-              <div>
-                <Icon>
-                  <a href=" ">
-                    <button type="button" class="emergencyLinesBtn">
-                      <p class="emergencyText">
-                        EMERGENCY AND CRISIS LINES
-                        <ion-icon class="phone" name="call">
-                        </ion-icon>
-                      </p>
-                    </button>
-                  </a>
-                </Icon>
-              </div>
-            </div>
+        <div class="lp-nav">
+          <a href="url">ABOUT </a>
+          <a href="url">CONTACT</a>
+        </div>
+        <div class="lp-flex-container">
+          <div>
+            <Icon>
+              <a href=" ">
+                <button type="button" class="searchDirectoryBtn">
+                  <p class="searchText">
+                    SEARCH DIRECTORY
+                    <ion-icon
+                      class="chevIconSearch"
+                      name="chevron-forward-outline"
+                    ></ion-icon>
+                  </p>
+                </button>
+              </a>
+            </Icon>
+          </div>
+
+          <div>
+            <Icon>
+              <a href=" ">
+                <button type="button" class="emergencyLinesBtn">
+                  <p class="emergencyText">
+                    EMERGENCY AND CRISIS LINES
+                    <ion-icon class="phone" name="call"></ion-icon>
+                  </p>
+                </button>
+              </a>
+            </Icon>
           </div>
         </div>
       </div>

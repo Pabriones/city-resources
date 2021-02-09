fb5670ded99da687865fe09c274c4d8f2b479eb8
MargaritaYatina <56407643+MargaritaYatina@users.noreply.github.com>
2021-02-08 16:39:00 -0800

    ContactPage styling changes

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Axios from "axios";
 import Modal from "react-modal";
 import { Grid } from "semantic-ui-react";
-import "../css/ContactPage.css";
 
-const ContactPage = () => {
+import "../../src/css/SearchDirectoryPage.css";
+
+const SearchDirectoryPage = () => {
   const [contactFirstName, setContactFirstName] = useState("");
   const [contactLastName, setContactLastName] = useState("");
   const [contactEmail, setContactEmail] = useState("");
@@ -33,27 +34,24 @@ const ContactPage = () => {
     setContactMessage("");
   };
 
-  return (
-    <div className="container">
-      <Grid divided="vertically">
-        <Grid.Row columns={1}>
-          <div className="ContactIntro">
+
+
+      return (
+       <div>
+         <div className="ContactIntro">
             <h1>We are here for you!</h1>
           </div>
-         
-          <div className="ContactIntro">
-            <p>Any questions or comments? <br></br>
+
+         <div class="ContactIntro2">
+         <p>Any questions or comments? <br></br>
             We would love to know your thoughts. If our<br></br>
             is no longer correct, or if there <br></br>
             are any other resources we should add,<br></br>
             please let us know!
             </p>
           </div>
-        </Grid.Row>
 
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <div className="SubmitForm">
+          <div className="SubmitForm">
               <form onSubmit={(e) => handleSubmit(e)}>
                 <div align="left" width="50%">
                   <div className="label">First Name: </div>
@@ -109,13 +107,10 @@ const ContactPage = () => {
               </div>
               <p>{successModalMsg}</p>
             </Modal>
-          </Grid.Column>
           
        
-        </Grid.Row>
-      </Grid>
     </div>
   );
 };
 
-export default ContactPage;
+export default SearchDirectoryPage;

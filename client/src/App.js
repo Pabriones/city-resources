63e9944233e0e4b1f1178e44c69265ea9b4afc90
MargaritaYatina <56407643+MargaritaYatina@users.noreply.github.com>
2021-02-11 11:19:52 -0800

    added some imports in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,12 @@ import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
 import AboutPage2 from "./components/AboutPage2";
 import Footer from "./components/Footer";
- 
+import ContactPage from "./components/ContactPage";
+import EmergencyPage from "./components/EmergencyPage";
+import SearchDirectoryPage from "./components/SearchDirectoryPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
 function App() {
   return (
    <div className= "cr-container">

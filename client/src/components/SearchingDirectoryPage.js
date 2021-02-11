94426c2eb817e28325c524dfb06024194eed914b
MargaritaYatina <56407643+MargaritaYatina@users.noreply.github.com>
2021-02-11 11:15:14 -0800

    searchingdirectory.js

--- client/src/components/SearchingDirectoryPage.js
+++ client/src/components/SearchingDirectoryPage.js
@@ -1,2 +1,19 @@
+import React from "react";
+import "../../src/css/SearchDirectoryPage.css";
+
+const SearchDirectoryPage = () => {
+  return (
+    <div>
+     
+         <div class="greeting">
+          <p>
+          Welcome to SearchDirectoryPage!
+          </p>
+          </div>
+    </div>
+  );
+};
+
+export default SearchDirectoryPage;
 
 

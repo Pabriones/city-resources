6493c30a81330b1c2b258a73552f46e8e4cbf65e
MargaritaYatina <56407643+MargaritaYatina@users.noreply.github.com>
2021-02-11 15:58:20 -0800

    kkk

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../../src/css/Footer.css";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import ContactPage from "./components/ContactPage";
+import EmergencyPage from "./components/EmergencyPage";
+import SearchDirectoryPage from "./components/SearchDirectoryPage";
 
 const Footer = () => {
   return (

fd457cb568e04d1d9b1e17e8726fc558f4c51013
selinapn <57070340+selinapn@users.noreply.github.com>
2021-02-27 20:18:59 -0800

    modalstyling

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -5,37 +5,37 @@ import "../../src/css/LoginModal.css";
 
 function LoginModal() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button type ="button" class="btn btn-link" onClick={handleShow}>
+      <Button type ="button" class="btn btn-link btn-info btn-lg" onClick={handleShow}>
         LOG IN
       </Button>
 
       <Modal show={show} onHide={handleClose} animation={false}>
-        <Modal.Header>
+     {/*    <Modal.Header>
           <Modal.Title>Log in here</Modal.Title>
-        </Modal.Header>
+        </Modal.Header> */}
 
         {/* Start of form for user to login */}
-        <Form>
+        
+        <div class="modal-dialog modal-lg">
         {/* Enter email address  */}
-        <Modal.Body>I am under construction</Modal.Body>
-        <Form.Group controlId="formBasicEmail">
+     
+        <div class= "form-group" controlId="formBasicEmail">
         <Form.Label>Email Address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
+        <Form.Control type="email" placeholder="Enter email" /></div>
+        
+        <div class ="form.group" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+        </div>
         <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Remember Me" />
         </Form.Group>
-        </Form>
+        </div>
         {/* End of form for users to login */}
 
         <Modal.Footer>

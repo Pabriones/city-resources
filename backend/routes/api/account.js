877aa285b210d81db1e78afd1249f6854ac9e484
Pabriones <Pabriones@users.noreply.github.com>
2021-04-06 00:19:27 -0700

    added user schema for database

--- backend/routes/api/account.js
+++ backend/routes/api/account.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/account
+// @route POST api/account
 // @desc Test route
 // @acess Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+	'/',
+	[
+		check('firstname', 'Firstname is required').not().isEmpty(),
+		check('lastname', 'Lastname is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 7 or more characters').isLength({
+			min: 7
+		})
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send('User route');
+	}
+);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,16 @@ const app = express();
 //connect database
 connectDB();
 
+//init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API RUNNING'));
 
 //define route
 app.use('/api/account', require('./routes/api/account'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 8080;
 

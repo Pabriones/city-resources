d99fffe87159b16245171065a8b18887ead45697
Pabriones <Pabriones@users.noreply.github.com>
2021-04-13 22:19:35 -0700

    added middleware auth that return users data if theres token

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Account = require('../../models/Account');
 
 // @route GET api/auth
 // @desc Test route
 // @acess Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+	try {
+		const account = await Account.findById(req.account.id).select('-password');
+		res.json(account);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;

d51dce74277ed33192060a51e7e71715ebfdaa7d
selinapn <57070340+selinapn@users.noreply.github.com>
2021-04-21 11:21:24 -0700

    footer, forms page, nav

--- client/src/components/AddResource.js
+++ client/src/components/AddResource.js
@@ -2,11 +2,11 @@ import React from "react";
 import "../../src/css/AddResource.css";
 import AddResourceForm from "./AddResourceForm";
 import Footer from "./Footer";
- 
+import TopNav2 from "./TopNav2";
 
 const AddResource = () => {
   return (
-    <div>
+    <div><TopNav2/>
       <div className="ar-container">
         <AddResourceForm />
       </div>
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import '../../src/css/Footer.css';
-import { Link as Link2 } from 'react-scroll';
-import { Link as Link1 } from 'react-router-dom';
+import React from "react";
+import "../../src/css/Footer.css";
+import { Link as Link2 } from "react-scroll";
+import { Link as Link1 } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -12,62 +12,98 @@ const Footer = () => {
           <div className="col-sm">
             <div className="ft-text">
               <Link1 className="ft-link-logo" to="/" spy={true} duration={500}>
-              CITY <br/> RESOURCES
+                CITY <br /> RESOURCES
               </Link1>
             </div>
           </div>
           {/* Column2 */}
+
           <div className="col-sm">
             <ui className="list-unstyled">
               <div className="ft-text">
+                {" "}
                 <li>
-                  <Link1 to className="ft-link" to="/">
-                    HOME
+				<Link1
+                  className="ft-link"
+                  to="/"
+                  spy={true}
+                  duration={500}
+                >
+                  ABOUT
+                </Link1>
+                </li>
+              </div>
+
+              <div className="ft-text">
+                {" "}
+                <li>
+				<Link1
+                  className="ft-link"
+                  to="/"
+                  spy={true}
+                  duration={500}
+                >
+                  CONTACT
+                </Link1>
+                </li>
+              </div>
+            </ui>
+          </div>
+
+          {/* Column3 */}
+          <div className="col-sm">
+            <ui className="list-unstyled">
+              <div className="ft-text">
+                {" "}
+                <li>
+                  <Link1
+                    className="ft-link"
+                    to="/directory"
+                    spy={true}
+                    duration={500}
+                  >
+                    {" "}
+                    SEARCH DIRECTORY{" "}
                   </Link1>
                 </li>
               </div>
-							<div className="ft-text">
-								{' '}
-								<li>
-									<Link1 className="ft-link" to="/directory" spy={true} duration={500}>
-										{' '}
-										SEARCH DIRECTORY{' '}
-									</Link1>
-								</li>
-							</div>
-						</ui>
-					</div>
+              <div className="ft-text">
+                <li>
+				<Link1 className="ft-link" to="/addresource" spy={true} duration={500}>
+                 LIST YOUR RESOURCE
+              </Link1>
+                </li>
+              </div>
+            </ui>
+          </div>
 
-					{/* Column3 */}
-					<div className="col-sm">
-						<ui className="list-unstyled">
-							<div className="ft-text">
-								{' '}
-								<li>
-									<Link2 className="ft-link" to="AboutPage" spy={true} duration={500}>
-										ABOUT
-									</Link2>
-								</li>
-							</div>
 
-							<div className="ft-text">
-								{' '}
-								<li>
-									<Link2 className="ft-link" to="ContactSection" spy={true} duration={500}>
-										CONTACT
-									</Link2>
-								</li>
-							</div>
-						</ui>
-					</div>
-				</div>
-				<div className="row">
-					<p className="col-lg">&copy;{new Date().getFullYear()} CITYRESOURCES</p>
-				</div>
-			</div>
-		</div>
-	);
+
+		  <div className="col-sm">
+            <ui className="list-unstyled">
+              <div className="ft-text">
+                {" "}
+                <li>
+				<Link1 className="ft-link" to="/forms" spy={true} duration={500}>
+                 FORMS
+              </Link1>
+                </li>
+              </div>
+
+            </ui>
+          </div>
+
+
+
+        </div>
+        <div className="row">
+          <p className="col-lg">
+            &copy;{new Date().getFullYear()} CITYRESOURCES
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
-

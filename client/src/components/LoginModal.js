1650ad5767a2e73b27b51f29d6cd7299c3de5ae1
selinapn <57070340+selinapn@users.noreply.github.com>
2021-04-28 18:15:53 -0700

    register link in modal

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { Modal, Button, Form, Container } from "react-bootstrap";
 import RegisterModal from "./RegisterModal";
 import "../../src/css/loginmodal.css";
-import { PersonCircle } from 'react-bootstrap-icons';
+import { PersonCircle } from "react-bootstrap-icons";
 import { Icon } from "semantic-ui-react";
-import Homepage from './Homepage';
+import Homepage from "./Homepage";
 import { Link as Link3 } from "react-router-dom";
-import RegisterPage from './RegisterPage'
+import RegisterPage from "./RegisterPage";
 import { Link as Link2 } from "react-router-dom";
 
 function LoginModal() {
@@ -18,26 +18,27 @@ function LoginModal() {
   return (
     <>
       <Button variant=" " className="nav-linksBtn" onClick={handleShow}>
-   SIGN IN 
+        SIGN IN
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <div className="logContainer" id="logContainer">
-         
           <div className="form-container sign-in-container">
             <form action="#">
               <h1>SIGN IN</h1>
+             <p>Don't have an account yet?<a href="/register" className="create-acct-link"> Create one!</a></p>
               <input type="email" placeholder="Email" />
               <input type="password" placeholder="Password" />
-              <a href="/resetpassword"> Forgot Password?</a>
-              <Link2 className="directLink" to="/homepage"> <button type="button" class="directLinkBtn">
+              <Link2 className="directLink" to="/homepage">
+                {" "}
+                <button type="button" class="directLinkBtn">
                   SIGN IN
-                        
-                        
-                      </button></Link2>      
+                </button>
+              </Link2>{" "}
+              <a href="/resetpassword"> Forgot Password?</a>
+  
             </form>
           </div>
-
 
           <h1>gsfsf</h1>
         </div>

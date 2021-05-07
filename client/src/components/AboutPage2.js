7575513d1444edb4a3502546c15cd11a3f215e84
selinapn <57070340+selinapn@users.noreply.github.com>
2021-05-06 19:45:39 -0700

    covid jumbo

--- client/src/components/AboutPage2.js
+++ client/src/components/AboutPage2.js
@@ -7,42 +7,15 @@ const AboutPage2 = () => {
       <div className="ab2-container">
         <div className="ab2-content">
           <div className="ab2-text-container">
-            <h1>
+            {/* <h1>
               With an increase in hardships due to the COVID-19 pandemic and the
               resulting economic recession, it has created new barriers for
               people already struggling.
-            </h1>
+            </h1> */}
 
-            <div className="ab2-title">
-              Mauris porttitor consequat ligula, nec porttitor ante commodo
-              cursus.
-            </div>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
-              quam aliquam, sodales ante id, porta odio. Curabitur tincidunt
-              tristique mauris quis placerat. Curabitur sit amet egestas ante, a
-              efficitur sem. Sed sed posuere libero. Cras eu velit vitae lorem
-              auctor molestie vel nec quam. Cras eget sem eget lorem posuere
-              facilisis. Proin luctus fermentum neque. Curabitur porttitor sem
-              sem, vitae laoreet lectus vehicula id. Vivamus lectus mauris,
-              rutrum vel malesuada quis, tincidunt a augue. Maecenas vel mattis
-              ligula. Proin fermentum nisi eu odio ornare, sed consectetur
-              ligula maximus.
-            </p>
+           
 
-            <div className="ab2-title">
-              Proin fermentum nisi eu odio ornare, sed consectetur ligula
-              maximus.
-            </div>
-
-            <p>
-              Proin luctus fermentum neque. Curabitur porttitor sem sem, vitae
-              laoreet lectus vehicula id. Vivamus lectus mauris, rutrum vel
-              malesuada quis, tincidunt a augue. Maecenas vel mattis ligula.
-              Proin fermentum nisi eu odio ornare, sed consectetur ligula
-              maximus.
-            </p>
           </div>
         </div>
       </div>
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,17 +1,65 @@
 import React from "react";
 import "../../src/css/LandingPage.css";
-
+import {
+  Container,
+  Jumbotron,
+  Accordion,
+  Card,
+  Form,
+  Button,
+} from "react-bootstrap";
 import Footer from "./Footer";
 import AboutPage from "./AboutPage";
 import AboutPage2 from "./AboutPage2";
 import ContactSection from "./ContactSection";
 import TopNav from "./TopNav";
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
 class LandingPage extends React.Component {
   render() {
     return (
       <body>
         <div id="LandingPage">
+          <Jumbotron fluid>
+            <Container className="lp-covid-jumbo">
+              <h3>
+                For the most update information about COVID-19, please visit the{" "}
+                <a
+                  href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
+                  className="lp-jumbo-link"
+                >
+                  CDC Website
+                </a>
+                .
+              </h3>
+              <p>
+                {" "}
+                <h4>Vaccine appointments are available!</h4>
+                King County:{" "}
+                <a
+                  href="https://seattle.signetic.com/home/93d64636-e4a2-eb11-b1ac-002248089768"
+                  className="lp-jumbo-link"
+                >
+                  Schedule an appointment
+                </a>
+                <br />
+                General vaccine locator:{" "}
+                <a
+                  href="https://vaccinelocator.doh.wa.gov/"
+                  className="lp-jumbo-link"
+                >
+                  Find an appointment
+                </a>{" "}
+                <br /> <br />
+                <a
+                  href="https://kingcounty.gov/depts/health/covid-19/~/media/depts/health/communicable-diseases/documents/C19/COVID-19-testing-locations.ashx"
+                  className="lp-jumbo-link"
+                >
+                  Free Covid-19 testing sites
+                </a>
+              </p>
+            </Container>
+          </Jumbotron>
           <div className="lp-container">
             <TopNav />
             <div className="lp-flex-container"></div>
@@ -19,7 +67,7 @@ class LandingPage extends React.Component {
         </div>
         <div>
           <AboutPage />
-          <AboutPage2 />
+          {/* <AboutPage2 /> */}
           <ContactSection />
           <Footer />
         </div>

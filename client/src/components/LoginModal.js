3e546bc169e83e36c83d10da9dbe3464c4fa73ce
selinapn <57070340+selinapn@users.noreply.github.com>
2021-05-14 22:59:22 -0700

    show pw and edits

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Row, Col } from "react-bootstrap";
 import "../../src/css/loginmodal.css";
 import { Link, Redirect } from "react-router-dom";
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {login} from '../actions/auth'
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../actions/auth";
 
-
-const LoginModal = ({login, isAuthenticated}) => {
+const LoginModal = ({ login, isAuthenticated }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -16,6 +15,11 @@ const LoginModal = ({login, isAuthenticated}) => {
     email: "",
     password: "",
   });
+
+  const [passwordShown, setPasswordShow] = useState(false);
+  const togglePassword = () => {
+    setPasswordShow(!passwordShown);
+  };
 
   const { email, password } = formData;
   const onChange = (e) =>
@@ -27,8 +31,8 @@ const LoginModal = ({login, isAuthenticated}) => {
   };
 
   // Redirect if logged in
-  if(isAuthenticated){
-    return <Redirect to='/homepage'/>
+  if (isAuthenticated) {
+    return <Redirect to="/homepage" />;
   }
 
   return (
@@ -38,52 +42,58 @@ const LoginModal = ({login, isAuthenticated}) => {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <div className="logContainer">
-          <div className="form-container sign-in-container">
-            <form action="#" onSubmit={(e) => onSubmit(e)}>
-              <h1>SIGN IN</h1>
-              <h6>
-                {" "}
-                Don't have an account yet?{" "}
-                <Link to="/registerpage">CREATE ONE!</Link>
-              </h6>
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={(e) => onChange(e)}
-                required
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={(e) => onChange(e)}
-                required
-              />
-               
-                <Link to="/resetpassword">Forgot Password?</Link>
-             
-
-              <button className="Login-modal-btn">
-                <Link>Sign in</Link>
-              </button>
-            </form>
-          </div>
+        {/* <div className="logContainer"> */}
+        <div className="form-container  ">
+          <form action="#" onSubmit={(e) => onSubmit(e)}>
+            {" "}
+            <h1>SIGN IN</h1>
+            <label className="email-pw-label">Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <label className="email-pw-label">
+              Password{" "}
+              <Link className="forgot-link" to="/resetpassword">
+                Forgot Password?
+              </Link>
+            </label>
+            <input
+              type={passwordShown ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <Button className="showPw-btn" onClick={togglePassword}>
+              Show Password
+            </Button>
+            <button className="Login-modal-btn">
+              <Link>Sign in</Link>
+            </button>
+            <p>
+              Don't have an account yet?{" "}
+              <Link to="/registerpage">CREATE ONE!</Link>
+            </p>
+          </form>
         </div>
+        {/* </div> */}
       </Modal>
     </>
   );
 };
 LoginModal.propTypes = {
   login: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
-export default connect(mapStateToProps, {login}) (LoginModal);
+export default connect(mapStateToProps, { login })(LoginModal);

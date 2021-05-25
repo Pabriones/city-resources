6937399459bef70c7a578fb2914cdc5654af32fe
selinapn <57070340+selinapn@users.noreply.github.com>
2021-05-24 22:16:17 -0700

    top nav homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
-import Footer from './Footer';
-import AboutPage from './AboutPage';
-import AboutPage2 from './AboutPage2';
-import ContactSection from './ContactSection';
-import '../../src/css/LandingPage.css';
-import TopNavHp from './TopNavHp';
-import CovidMain from './CovidMain';
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import Footer from "./Footer";
+import AboutPage from "./AboutPage";
+import AboutPage2 from "./AboutPage2";
+import ContactSection from "./ContactSection";
+import "../../src/css/LandingPage.css";
+import TopNavHp from "./TopNavHp";
+import CovidMain from "./CovidMain";
 
 class Homepage extends React.Component {
-	render() {
-		return (
-			<body>
-				<div id="LandingPage">
-					<Jumbotron fluid>
-						<Container className="lp-covid-jumbo">
-							<CovidMain />
-						</Container>
-					</Jumbotron>
-					<div className="lp-container">
-						<TopNavHp />
-					</div>
-				</div>
-				<div>
-					<AboutPage />
-					<AboutPage2 />
-					<ContactSection />
-					<Footer />
-				</div>
-			</body>
-		);
-	}
+  render() {
+    return (
+      <body>
+        <div id="LandingPage">
+          <CovidMain />
+
+          <div className="lp-container">
+            <TopNavHp />
+          </div>
+        </div>
+        <div>
+          <AboutPage />
+     
+          <ContactSection />
+          <Footer />
+        </div>
+      </body>
+    );
+  }
 }
 
 export default Homepage;

2495b2d7fbd4de170ff65a09e2a6d2c95daf59da
selinapn <57070340+selinapn@users.noreply.github.com>
2021-06-07 22:07:49 -0700

    resetpw with  emailjs

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,47 +1,54 @@
-import React from 'react';
-import '../../src/css/LandingPage.css';
-import { Container, Jumbotron, Accordion, Card, Form, Button } from 'react-bootstrap';
-import Footer from './Footer';
-import AboutPage from './AboutPage';
-import AboutPage2 from './AboutPage2';
-import ContactSection from './ContactSection';
-import TopNav from './TopNav';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import CovidMain from './CovidMain';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Redirect } from 'react-router';
+import React from "react";
+import "../../src/css/LandingPage.css";
+import {
+  Container,
+  Jumbotron,
+  Accordion,
+  Card,
+  Form,
+  Button,
+} from "react-bootstrap";
+import Footer from "./Footer";
+import AboutPage from "./AboutPage";
+import AboutPage2 from "./AboutPage2";
+import ContactSection from "./ContactSection";
+import TopNav from "./TopNav";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import CovidMain from "./CovidMain";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router";
 
 const LandingPage = ({ isAuthenticated }) => {
-	if (isAuthenticated) {
-		return <Redirect to="/homepage" />;
-	}
+  if (isAuthenticated) {
+    return <Redirect to="/homepage" />;
+  }
 
-	return (
-		<body>
-			<div id="LandingPage">
-				<CovidMain />
+  return (
+    <body>
+      <div id="LandingPage">
+        <CovidMain />
 
-				<div className="lp-container">
-					<TopNav />
-					<div className="lp-flex-container" />
-				</div>
-			</div>
-			<div>
-				<AboutPage />
-				<ContactSection />
-				<Footer />
-			</div>
-		</body>
-	);
+        <div className="lp-container">
+          <TopNav />
+          <div className="lp-flex-container" />
+        </div>
+      </div>
+      <div>
+        <AboutPage />
+        <ContactSection />
+        <Footer />
+      </div>
+    </body>
+  );
 };
 
 LandingPage.propTypes = {
-	isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
-	isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps)(LandingPage);

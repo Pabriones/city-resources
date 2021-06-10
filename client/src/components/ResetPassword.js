9f34cb10af6ef19d235c54addd2b91fef7de607c
Pabriones <Pabriones@users.noreply.github.com>
2021-06-10 05:07:44 -0700

    just simple modification

--- client/src/components/ResetPassword.js
+++ client/src/components/ResetPassword.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "../../src/css/ResetPassword.css";
-import Footer from "./Footer";
-import emailjs from "emailjs-com";
-import TopNav2 from "./TopNav2";
-import { Button, Form, Row, Container, Col } from "react-bootstrap";
-import { ToastContainer, toast, Bounce } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import '../../src/css/ResetPassword.css';
+import Footer from './Footer';
+import emailjs from 'emailjs-com';
+import TopNav2 from './TopNav2';
+import { Button, Form, Row, Container, Col } from 'react-bootstrap';
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // function EnableSend() {
 //   document.getElementById("submit").disabled = false;
@@ -14,81 +14,72 @@ import "react-toastify/dist/ReactToastify.css";
 //   document.getElementById("submit").innerHTML = "Message sent.";
 // }
 
- 
-
 function ResetPassword() {
-  const notify = () => {
-    toast.success("Instructions sent!", {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
+	const notify = () => {
+		toast.success('Instructions sent!', {
+			position: 'top-center',
+			autoClose: 3000,
+			hideProgressBar: true,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined
+		});
+	};
 
-  function resetPw(e) {
-    e.preventDefault();
+	function resetPw(e) {
+		e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_wkyp2uz",
-        "template_yrygo4o",
-        e.target,
-        "user_HarQvCQDVRSyllfUxOCDt"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  }
+		emailjs.sendForm('service_wkyp2uz', 'template_yrygo4o', e.target, 'user_HarQvCQDVRSyllfUxOCDt').then(
+			(result) => {
+				console.log(result.text);
+			},
+			(error) => {
+				console.log(error.text);
+			}
+		);
+		e.target.reset();
+	}
 
-  return (
-    <body>
-      {" "}
-      <TopNav2 />{" "}
-      <div className="rpass-container">
-        <div className="reset-text">
-          <h1>Reset Password</h1>
-          <p>
-            Enter the email associated with your account and we'll send you an
-            email with instructions to reset your password.
-          </p>
-        </div>{" "}
-        <form onSubmit={resetPw}>
-          <div className="row   ">
-            <div className=" form-group pt-2 mx-auto rpass-email">
-              <input
-                type="email"
-                className="form-control shadow-none rp-email-input"
-                required="true"
-                placeholder="Email Address"
-                name="email"
-              />
-            </div>
+	return (
+		<body>
+			{' '}
+			<TopNav2 />{' '}
+			<div className="rpass-container">
+				<div className="reset-text">
+					<h1>Reset Password</h1>
+					<p>
+						Enter the email associated with your account and we'll send you an email with instructions to
+						reset your password.
+					</p>
+				</div>{' '}
+				<form onSubmit={resetPw}>
+					<div className="row   ">
+						<div className=" form-group pt-2 mx-auto rpass-email">
+							<input
+								type="email"
+								className="form-control shadow-none rp-email-input"
+								required="true"
+								placeholder="Email Address"
+								name="email"
+							/>
+						</div>
 
-            <div className="reset-submit">
-              <button
-                type="submit"
-                className="reset-submitBtn"
-                id="submit"
-                name="submit"
-                onClick={notify}
-              >
-                Send Instructions
-              </button>
-              <ToastContainer className="toast-alert" />
-            </div>
-          </div>
-        </form>
-        {/* <form>
+						<div className="reset-submit">
+							<button
+								type="submit"
+								className="reset-submitBtn"
+								id="submit"
+								name="submit"
+								onClick={notify}
+							>
+								Send Instructions
+							</button>
+							<ToastContainer className="toast-alert" />
+						</div>
+					</div>
+				</form>
+				{/* <form>
           <div className="reset-text">
             <h1>Reset Password</h1>
             <p>
@@ -124,10 +115,10 @@ function ResetPassword() {
             </div>
           </div>
         </form> */}
-      </div>{" "}
-      <Footer />
-    </body>
-  );
+			</div>{' '}
+			<Footer />
+		</body>
+	);
 }
 
 export default ResetPassword;
